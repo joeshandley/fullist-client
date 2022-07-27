55ac2147f682a3963ca3900dc604a8872aec545f
Joe Shandley <josephshandley@gmail.com>
2022-07-27 17:16:18 +0100

    Add variables and create first logo draft

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.scss";
+import logo from "./assets/logos/logo.svg";
 
 function App() {
   return (
     <>
       <h1>Test</h1>
       <p>Add content here</p>
+      <div className="logo-container">
+        <h2>F</h2>
+        <img src={logo} alt="" />
+        <h2>llist.</h2>
+      </div>
     </>
   );
 }
