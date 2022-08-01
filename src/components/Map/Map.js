da3a4a982bb077574095d93343899594ba0bf4b9
Joe Shandley <josephshandley@gmail.com>
2022-08-01 11:55:12 +0100

    Add clickable locations and custom markers

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,6 +6,8 @@ import mapboxgl from "mapbox-gl";
 
 import "./Map.scss";
 
+// Much of the code for this component was adapted from https://docs.mapbox.com/help/tutorials/building-a-store-locator
+
 export default function () {
   // TODO: use token from .env file
   // mapboxgl.accessToken = process.env.MAP_TOKEN;
@@ -112,9 +114,131 @@ export default function () {
       },
     ],
   };
+  // TODO: add an id into the data myself?
+  // Although, if I want to make this dynamic and have a proximity algorithm, maybe no hard code. Can do both?
   shops.features.forEach(function (shop, i) {
     shop.properties.id = i;
   });
+
+  const buildLocationList = (shops, map) => {
+    for (const shop of shops.features) {
+      /* Add a new listing section to the sidebar. */
+      const listings = document.getElementById("listings");
+      const listing = listings.appendChild(document.createElement("div"));
+      /* Assign a unique `id` to the listing. */
+      listing.id = `listing-${shop.properties.id}`;
+      /* Assign the `item` class to each listing for styling. */
+      listing.className = "item";
+
+      /* Add the link to the individual listing created above. */
+      const link = listing.appendChild(document.createElement("a"));
+      link.href = "#";
+      link.className = "title";
+      link.id = `link-${shop.properties.id}`;
+      link.innerHTML = `${shop.properties.address}`;
+      link.addEventListener("click", function () {
+        for (const feature of shops.features) {
+          if (this.id === `link-${feature.properties.id}`) {
+            flyToShop(feature, map);
+            createPopUp(feature, map);
+          }
+        }
+        const activeItem = document.getElementsByClassName("active");
+        if (activeItem[0]) {
+          activeItem[0].classList.remove("active");
+        }
+        this.parentNode.classList.add("active");
+      });
+
+      /* Add details to the individual listing. */
+      const details = listing.appendChild(document.createElement("div"));
+      details.innerHTML = `${shop.properties.city}`;
+      if (shop.properties.phone) {
+        details.innerHTML += ` · ${shop.properties.phoneFormatted}`;
+      }
+      // TODO: use the distance calculation when that is needed
+      if (shop.properties.distance) {
+        const roundedDistance =
+          Math.round(shop.properties.distance * 100) / 100;
+        details.innerHTML += `<div><strong>${roundedDistance} miles away</strong></div>`;
+      }
+    }
+  };
+
+  function flyToShop(location, map) {
+    map.flyTo({
+      center: location.geometry.coordinates,
+      zoom: 15,
+    });
+  }
+
+  function createPopUp(location, map) {
+    const popUps = document.getElementsByClassName("mapboxgl-popup");
+    /** Check if there is already a popup on the map and if so, remove it */
+    if (popUps[0]) popUps[0].remove();
+
+    const popup = new mapboxgl.Popup({ closeOnClick: false })
+      .setLngLat(location.geometry.coordinates)
+      .setHTML(`<h3>Sweetgreen</h3><h4>${location.properties.address}</h4>`)
+      .addTo(map);
+  }
+
+  // TODO: use this function for distance between two points
+  function getDistanceInKm(lat1, lng1, lat2, lng2) {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2 - lat1); // deg2rad below
+    const dLng = deg2rad(lng2 - lng1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLng / 2) *
+        Math.sin(dLng / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    return d;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+  }
+
+  function addMarkers(map) {
+    /* For each feature in the GeoJSON object above: */
+    for (const marker of shops.features) {
+      /* Create a div element for the marker. */
+      const el = document.createElement("div");
+      /* Assign a unique `id` to the marker. */
+      el.id = `marker-${marker.properties.id}`;
+      /* Assign the `marker` class to each marker for styling. */
+      el.className = "marker";
+
+      el.addEventListener("click", (e) => {
+        /* Fly to the point */
+        flyToShop(marker, map);
+        /* Close all other popups and display popup for clicked store */
+        createPopUp(marker, map);
+        /* Highlight listing in sidebar */
+        const activeItem = document.getElementsByClassName("active");
+        e.stopPropagation();
+        if (activeItem[0]) {
+          activeItem[0].classList.remove("active");
+        }
+        const listing = document.getElementById(
+          `listing-${marker.properties.id}`
+        );
+        listing.classList.add("active");
+      });
+
+      /**
+       * Create a marker using the div element
+       * defined above and add it to the map.
+       **/
+      new mapboxgl.Marker(el, { offset: [0, -23] })
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+    }
+  }
 
   // Create map on page load
   useEffect(() => {
@@ -128,19 +252,40 @@ export default function () {
     map.addControl(new mapboxgl.NavigationControl());
 
     map.on("load", () => {
-      /* Add the data to your map as a layer */
-      map.addLayer({
-        id: "locations",
-        type: "circle",
-        /* Add a GeoJSON source containing place coordinates and information. */
-        source: {
-          type: "geojson",
-          data: shops,
-        },
+      map.addSource("places", {
+        type: "geojson",
+        data: shops,
       });
     });
 
-    // Add animation when user enters postcode
+    addMarkers(map);
+
+    // map.on("click", (event) => {
+    //   /* Determine if a feature in the "locations" layer exists at that point. */
+    //   const features = map.queryRenderedFeatures(event.point, {
+    //     layers: ["locations"],
+    //   });
+    //   if (!features.length) return;
+
+    //   const clickedPoint = features[0];
+
+    //   flyToShop(clickedPoint, map);
+    //   createPopUp(clickedPoint, map);
+
+    //   /* Highlight listing in sidebar (and remove highlight for all other listings) */
+    //   const activeItem = document.getElementsByClassName("active");
+    //   if (activeItem[0]) {
+    //     activeItem[0].classList.remove("active");
+    //   }
+    //   const listing = document.getElementById(
+    //     `listing-${clickedPoint.properties.id}`
+    //   );
+    //   listing.classList.add("active");
+    // });
+
+    buildLocationList(shops, map);
+
+    // Add animation to fly to the user-entered postcode
     document
       .getElementById("postcode-form")
       .addEventListener("submit", async (e) => {
@@ -156,14 +301,14 @@ export default function () {
                 postcodeData.data.result.latitude,
               ],
               zoom: 12,
-              essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+              essential: true,
             });
           }
         } catch (err) {
           console.log(`Error: `, err);
         }
       });
-  }); // TODO: add back this dependency for using user location? [lng, lat, zoom]
+  });
 
   return (
     // TODO: change class names
