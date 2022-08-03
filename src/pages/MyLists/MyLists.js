993e8d856ce0f90a48116bfe52ded2aa36c17f0a
Joe Shandley <josephshandley@gmail.com>
2022-08-03 12:45:08 +0100

    Add list nav and editable name field

--- src/pages/MyLists/MyLists.js
+++ src/pages/MyLists/MyLists.js
@@ -1,7 +1,30 @@
-import React from "react";
+import { EditText } from "react-edit-text";
+import "react-edit-text/dist/index.css";
+
+import "./MyLists.scss";
 
 const MyLists = () => {
-  return <div>MyLists</div>;
+  return (
+    <main className="lists">
+      <nav className="lists__nav">
+        <a className="lists__nav-item" href="/lists/add">
+          + New List
+        </a>
+        <a className="lists__nav-item" href="/lists/favourites">
+          Favourites
+        </a>
+        <a className="lists__nav-item" href="/lists/all">
+          Previous Lists
+        </a>
+      </nav>
+      <EditText
+        name="list-name"
+        defaultValue="Enter your list name"
+        editButtonProps={{ style: { marginLeft: "5px" } }}
+        showEditButton
+      />
+    </main>
+  );
 };
 
 export default MyLists;
