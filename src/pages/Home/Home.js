c23a1a2db8d8d7400c5212ea6d2438ef2bab009d
Joe Shandley <josephshandley@gmail.com>
2022-08-03 12:09:05 +0100

    Add image carousels

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+// Import Swiper dependencies
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
 import heroImage from "../../assets/images/hero-image.jpeg";
+import groceries from "../../assets/images/groceries.jpeg";
+import groceryBasket from "../../assets/images/grocery-basket.jpeg";
 import "./Home.scss";
 
 const Home = () => {
@@ -8,6 +16,103 @@ const Home = () => {
       <div className="home__hero">
         <img src={heroImage} alt="Shopping basket" className="home__hero-img" />
         <h1 className="home__hero-text">One trip.&nbsp; No fuss.</h1>
+      </div>
+      <div className="home__list-buttons">
+        <a class="home__list-button" href="/lists/add">
+          + Add New List
+        </a>
+        <a class="home__list-button" href="/lists">
+          {/* TODO: In future, get id to link straight to user's latest list */}
+          View Previous Lists
+        </a>
+      </div>
+      <div className="home__new-in">
+        <h2 className="home__subtitle">New In</h2>
+        {/* TODO: fix carousel */}
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={80}
+          // pagination={{
+          //   clickable: true,
+          // }}
+          modules={[Pagination]}
+          className="home__slides"
+        >
+          <SwiperSlide>
+            <div className="home__slide">
+              <img
+                src={groceryBasket}
+                alt="grocery item"
+                className="home__slide-image"
+              />
+              {/* <p className="home__slides-text">Stitching repairs</p> */}
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="home__slide">
+              <img
+                src={groceryBasket}
+                alt="grocery item"
+                className="home__slide-image"
+              />
+              {/* <p className="home__slides-text">Stitching repairs</p> */}
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="home__slide">
+              <img
+                src={groceryBasket}
+                alt="grocery item"
+                className="home__slide-image"
+              />
+              {/* <p className="home__slides-text">Stitching repairs</p> */}
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="home__recipes">
+        <h2 className="home__subtitle">Recipes</h2>
+        {/* TODO: fix carousel */}
+        <Swiper
+          slidesPerView={"auto"}
+          //   spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="home__slides"
+        >
+          <SwiperSlide>
+            <div className="home__slide">
+              <img
+                src={groceries}
+                alt="grocery item"
+                className="home__slide-image"
+              />
+              {/* <p className="home__slides-text">Stitching repairs</p> */}
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="home__slide">
+              <img
+                src={groceries}
+                alt="grocery item"
+                className="home__slide-image"
+              />
+              {/* <p className="home__slides-text">Stitching repairs</p> */}
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="home__slide">
+              <img
+                src={groceries}
+                alt="grocery item"
+                className="home__slide-image"
+              />
+              {/* <p className="home__slides-text">Stitching repairs</p> */}
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </main>
   );
