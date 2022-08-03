6e324ddb7867515fb72262e5f5d2e090649d4842
Joe Shandley <josephshandley@gmail.com>
2022-08-03 10:37:19 +0100

    Add hero banner

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <div className="logo__container">
       <h2 className="logo__text">F</h2>
-      <img className="logo__image" src={logo} alt="Fullist logo" />
+      <img className="logo__image" src={logo} alt="Fullist logo image" />
       <h2 className="logo__text">llist.</h2>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import heroImage from "../../assets/images/hero-image.jpeg";
+import "./Home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="home">
+      <div className="home__hero">
+        <img src={heroImage} alt="Shopping basket" className="home__hero-img" />
+        <h1 className="home__hero-text">One trip.&nbsp; No fuss.</h1>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
