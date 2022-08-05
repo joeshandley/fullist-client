c66da4a3e4a68dc0bc01a2eea6a1ece0f35a637a
Joe Shandley <josephshandley@gmail.com>
2022-08-05 11:15:23 +0100

    Implement adding new list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import MyLists from "./pages/MyLists/MyLists";
+import AddList from "./pages/AddList/AddList";
 import List from "./pages/List/List";
 import Locations from "./pages/Locations/Locations";
 import "./App.scss";
@@ -14,6 +15,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/lists" exact component={MyLists} />
+        <Route path="/lists/add" exact component={AddList} />
         <Route
           path="/lists/:id"
           render={(routerProps) => <List {...routerProps} />}
---
+++ src/pages/AddList/AddList.js
@@ -0,0 +1,186 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { EditText } from "react-edit-text";
+import "react-edit-text/dist/index.css";
+import ListItem from "../../components/ListItem/ListItem";
+import AddListItem from "../../components/AddListItem/AddListItem";
+import VariableArrow from "../../components/VariableArrow/VariableArrow";
+import "./AddList.scss";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+const AddList = () => {
+  const [list, setList] = useState([]);
+  const [listId, setListId] = useState("");
+  const [itemList, setItemList] = useState([]);
+  const [isUserAddingItem, setIsUserAddingItem] = useState(false);
+  const [isItemAdded, setItemAdded] = useState(true);
+
+  const addList = async () => {
+    try {
+      const response = await axios.post(`${BACKEND_URL}/lists`);
+      console.log(response);
+      if (response) {
+        setList(response.data.newListCreated);
+        setListId(response.data.newListCreated.id);
+        setItemAdded(false);
+      }
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  };
+
+  const getList = async () => {
+    try {
+      const response = await axios.get(`${BACKEND_URL}/lists/${listId}`);
+      if (response) {
+        console.log(response);
+        if (response.data.items.length === 0) {
+          return;
+        }
+        const itemList = response.data.items.map((item) => {
+          return (
+            <ListItem
+              key={item.id}
+              id={item.id}
+              quantity={item.quantity}
+              name={item.name}
+              editItemHandler={editItem}
+              deleteItemHandler={deleteItemHandler}
+            />
+          );
+        });
+        setList(response.data);
+        setItemList(itemList);
+        setItemAdded(false);
+      }
+    } catch (err) {
+      console.log(`Error: `, err);
+    }
+  };
+
+  const editListName = async (e) => {
+    try {
+      const response = await axios.patch(`${BACKEND_URL}/lists/${listId}`, {
+        name: e.value,
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(`Error: `, err);
+    }
+  };
+
+  const editItem = async (e, itemId) => {
+    try {
+      if (e.name === "quantity") {
+        const response = await axios.patch(
+          `${BACKEND_URL}/lists/${listId}/${itemId}`,
+          { quantity: e.value }
+        );
+        console.log(response);
+      }
+      if (e.name === "itemName") {
+        const response = await axios.patch(
+          `${BACKEND_URL}/lists/${listId}/${itemId}`,
+          { name: e.value }
+        );
+        console.log(response);
+      }
+    } catch (err) {
+      console.log(`Error: `, err);
+    }
+  };
+
+  const addItemHandler = async (e) => {
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/lists/${listId}/add-item`,
+        {
+          name: e.value,
+          quantity: "1",
+        }
+      );
+      setIsUserAddingItem(false);
+      setItemAdded(true);
+      console.log(response);
+    } catch (err) {
+      console.log(`Error: `, err);
+    }
+  };
+
+  const deleteItemHandler = async (e) => {
+    try {
+      const response = await axios.delete(
+        `${BACKEND_URL}/lists/${listId}/${e.target.parentNode.id}`
+      );
+      console.log(response);
+      getList();
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    addList();
+  }, []);
+
+  useEffect(() => {
+    if (listId !== "") {
+      if (isItemAdded) {
+        getList();
+      }
+    }
+  }, [isItemAdded]);
+
+  return (
+    <main className="list">
+      <EditText
+        className="list__name"
+        name="list-name"
+        placeholder="Enter your list name"
+        defaultValue={list.name}
+        onSave={(e) => {
+          editListName(e);
+        }}
+        showEditButton
+        style={{ width: "100%", fontSize: "2.4rem" }}
+        editButtonProps={{
+          style: {
+            marginLeft: "5px",
+            backgroundColor: "#fbfbfb",
+            fill: "#1c0f13",
+            cursor: "pointer",
+          },
+        }}
+      />
+      <div className="list__container">{itemList}</div>
+      <div
+        className={`list__item${
+          isUserAddingItem ? " list__item--show" : " list__item--hide"
+        }`}
+      >
+        <AddListItem
+          addItemHandler={addItemHandler}
+          isAddingItem={isUserAddingItem}
+          deleteHandler={() => {
+            setIsUserAddingItem(false);
+          }}
+        />
+      </div>
+      <p
+        className="list__add-item"
+        onClick={() => {
+          setIsUserAddingItem(true);
+        }}
+      >
+        + Add item to list
+      </p>
+      <div className="list__check-nearby">
+        <h3 className="list__check-title">Check your list nearby...</h3>
+        <VariableArrow listId={list.id} />
+      </div>
+    </main>
+  );
+};
+
+export default AddList;
