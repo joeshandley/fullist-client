e14c907aba0416a8e2d53435d19fa0a79948c66d
Joe Shandley <josephshandley@gmail.com>
2022-08-08 11:04:55 +0100

    Add carousel image props

--- src/components/CarouselImage/CarouselImage.js
+++ src/components/CarouselImage/CarouselImage.js
@@ -3,12 +3,12 @@ import { SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "./CarouselImage.scss";
 
-const CarouselImage = () => {
+const CarouselImage = ({ imgSrc, altText, imgText }) => {
   return (
     <SwiperSlide>
       <div className="home__slide">
-        <img src={groceries} alt="grocery item" className="home__slide-image" />
-        <p className="home__slide-text">Spaghetti Carbonara</p>
+        <img src={imgSrc} alt={altText} className="home__slide-image" />
+        <p className="home__slide-text">{imgText}</p>
       </div>
     </SwiperSlide>
   );
