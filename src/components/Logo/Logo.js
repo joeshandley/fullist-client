642c45c0ee365c3f61d6ad4daf3ba1651c5c4dfc
Joe Shandley <josephshandley@gmail.com>
2022-08-09 00:55:51 +0100

    Add basket logo animation on hover

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import logo from "../../assets/logos/logo.svg";
+import "animate.css";
 import "./Logo.scss";
 
 export default function Logo() {
   return (
     <div className="logo__container">
       <h2 className="logo__text">F</h2>
-      <img className="logo__image" src={logo} alt="Fullist logo image" />
+      <img className="logo__image " src={logo} alt="Fullist logo image" />
       <h2 className="logo__text">llist.</h2>
     </div>
   );
